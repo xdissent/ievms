a377220458b204215a680a98e21d6eb116436a8c
Greg Thornton <xdissent@me.com>
2012-09-23 17:02:46 -0500

    Only download GA ISO if it doesn't exist.

--- ievms.sh
+++ ievms.sh
@@ -215,13 +215,17 @@ build_ievm() {
 
         if [[ ! -f "${ga_iso}" ]]
         then
-            check_version
-            url="http://download.virtualbox.org/virtualbox/${major_minor_release}/VBoxGuestAdditions_${major_minor_release}.iso"
             ga_iso="${ievms_home}/VBoxGuestAdditions.iso"
-            log "Downloading Virtualbox Guest Additions ISO from ${url} to ${ga_iso}"
-            if ! curl ${curl_opts} -L "${url}" -o "${ga_iso}"
+
+            if [[ ! -f "${ga_iso}" ]]
             then
-                fail "Failed to download ${url} to ${ga_iso} using 'curl', error code ($?)"
+                check_version
+                url="http://download.virtualbox.org/virtualbox/${major_minor_release}/VBoxGuestAdditions_${major_minor_release}.iso"
+                log "Downloading Virtualbox Guest Additions ISO from ${url} to ${ga_iso}"
+                if ! curl ${curl_opts} -L "${url}" -o "${ga_iso}"
+                then
+                    fail "Failed to download ${url} to ${ga_iso} using 'curl', error code ($?)"
+                fi
             fi
         fi
 
