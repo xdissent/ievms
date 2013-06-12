4bb7cd5b112250de85a45aef5602639ffe6c709a
Greg Thornton <xdissent@me.com>
2013-06-12 09:04:51 -0500

    GA for all

---
+++ control/vboxga.bat
@@ -0,0 +1,8 @@
+cd /d D:\
+cd \cert
+VBoxCertUtil.exe add-trusted-publisher oracle-vbox.cer --root oracle-vbox.cer
+cd \
+VBoxWindowsAdditions.exe /S
+regedit.exe /S C:\reuac.reg
+shutdown.exe /s /t 00
+del %0
