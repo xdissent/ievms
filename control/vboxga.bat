753180127e1fe00c5233600fceebecaf7e55b024
Greg Thornton <gthornton@cjadvertising.com>
2013-06-12 10:52:17 -0500

    Remove reuac.reg after it's imported

--- control/vboxga.bat
+++ control/vboxga.bat
@@ -4,5 +4,6 @@ VBoxCertUtil.exe add-trusted-publisher oracle-vbox.cer --root oracle-vbox.cer
 cd \
 VBoxWindowsAdditions.exe /S
 regedit.exe /S C:\reuac.reg
+del C:\reuac.reg
 shutdown.exe /s /t 00
 del %0
